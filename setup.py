0dd8a317a945e99b87b4fee17dcf2a1e1c931f4f
ortz <or@tzabary.com>
2022-11-22 15:09:51 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
+#!/usr/bin/env python
+
 from setuptools import setup
+
+with open('README.md') as file:
+    long_description = file.read()
 
 setup(
     name='lakefs-playground-utils',
     version='0.0.1',
+    description = 'lakeFS Playground Utilities',
+    author = 'Or Tzabary',
+    author_email = 'or.tzabary@treeverse.io',
+    url = 'https://github.com/treeverse/lakefs-playground-utils',
+    license = 'Apache 2.0',
+    keywords = 'lakefs playground',
+    platforms = ['any'],
+    long_description = long_description,
     install_requires=[
-	'fsspec>=2021.11.0',
-	'requests>=2.0.0'
-	'lakefs-client==0.85.0',
+        'fsspec>=2021.11.0',
+        'requests>=2.0.0'
+        'lakefs-client==0.85.0',
     ],
 )
