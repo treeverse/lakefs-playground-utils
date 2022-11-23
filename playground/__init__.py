ca890d963eb09a9ee3a997f68b1729aaf259bf26
Barak Amar <barak.amar@treeverse.io>
2022-11-23 10:25:10 +0200

    format and misc fixes

--- playground/__init__.py
+++ playground/__init__.py
@@ -1,29 +1,32 @@
 from .config import PlaygroundDetails
 from .fs import register_fs
 
-PLAYGROUND_CONTROL_PLANE_URL = 'https://demo-ort.lakefs.dev/api/v1/notebook'
+PLAYGROUND_CONTROL_PLANE_URL = "https://demo-ort.lakefs.dev/api/v1/notebook"
 
 
 class LakeFSPlaygroundError(RuntimeError):
     pass
 
+
 def get_or_create(email: str) -> PlaygroundDetails:
     import requests
     import base64
     import json
-    resp = requests.post(PLAYGROUND_CONTROL_PLANE_URL, params={'email': email})
+
+    resp = requests.post(PLAYGROUND_CONTROL_PLANE_URL, params={"email": email})
     if resp.status_code != 200:
-        raise LakeFSPlaygroundError(f'HTTP {resp.status_code}: {resp.text}')
+        raise LakeFSPlaygroundError(f"HTTP {resp.status_code}: {resp.text}")
 
     # Decode Message
     message_bytes = base64.b64decode(resp.content)
-    message = json.loads(message_bytes.decode('ascii'))
+    message = json.loads(message_bytes.decode("ascii"))
 
     return PlaygroundDetails(
-        access_key_id=message['LakeFSCreds']['AccessKeyID'],
-        secret_access_key=message['LakeFSCreds']['SecretAccessKey'],
-        endpoint_url=message['Host'],
+        access_key_id=message["LakeFSCreds"]["AccessKeyID"],
+        secret_access_key=message["LakeFSCreds"]["SecretAccessKey"],
+        endpoint_url=message["Host"],
     )
+
 
 def mount(details: PlaygroundDetails):
     register_fs(details=details)
--- playground/config.py
+++ playground/config.py
@@ -1,13 +1,14 @@
-
 class PlaygroundDetails:
     access_key_id: str
     secret_access_key: str
-    endpoint_url: str 
+    endpoint_url: str
 
-    def __init__(self, access_key_id: str, secret_access_key: str, endpoint_url: str) -> None:
+    def __init__(
+        self, access_key_id: str, secret_access_key: str, endpoint_url: str
+    ) -> None:
         self.access_key_id = access_key_id
         self.secret_access_key = secret_access_key
         self.endpoint_url = endpoint_url
 
     def __repr__(self):
-        return f'PlaygroundDetails(access_key_id={self.access_key_id}, secret_access_key={self.secret_access_key}, endpoint_url={self.endpoint_url})'
+        return f"PlaygroundDetails(access_key_id={self.access_key_id}, secret_access_key={self.secret_access_key}, endpoint_url={self.endpoint_url})"
