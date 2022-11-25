1224b313269caaec2590cb195fcd65db85ebdad9
Oz Katz <oz.katz@treeverse.io>
2022-11-25 14:54:12 +0200

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as file:
 
 setup(
     name = 'lakefs-playground-utils',
-    version = '0.0.7',
+    version = '0.0.8',
     description = 'lakeFS Playground Utilities',
     author = 'Or Tzabary',
     author_email = 'or.tzabary@treeverse.io',
